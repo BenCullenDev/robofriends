5c4761c0ffd0a8ca0495c5d2612fee69e78eb3d0
Ben Cullen <benjaminjcullen1@gmail.com>
2022-03-22 17:16:21 +0000

    Updates

---
+++ precache-manifest.c6bb1d0afc16a5ebb9cd0de96bbfa57e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aad1658eee3315834489",
+    "url": "/static/css/main.9d7faa66.chunk.css"
+  },
+  {
+    "revision": "aad1658eee3315834489",
+    "url": "/static/js/main.cea5fc6c.chunk.js"
+  },
+  {
+    "revision": "ebcd9eed7367ad94ebcf",
+    "url": "/static/js/runtime~main.012de3fb.js"
+  },
+  {
+    "revision": "fcb3c71b1d86aefb1fde",
+    "url": "/static/css/2.877a6d9b.chunk.css"
+  },
+  {
+    "revision": "fcb3c71b1d86aefb1fde",
+    "url": "/static/js/2.f2c8162a.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5cfbcdc3c9c27713c200f6e76dd5bdf4",
+    "url": "/index.html"
+  }
+];
